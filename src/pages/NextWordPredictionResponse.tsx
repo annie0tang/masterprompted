48aceabc658fc86a5664953b93daaf93add362ff
natesowd <natesowder2023@u.northwestern.edu>
2025-09-29 12:16:43 +0200

    sneaking in breadcrumb for NWS response

--- src/pages/NextWordPredictionResponse.tsx
+++ src/pages/NextWordPredictionResponse.tsx
@@ -170,7 +170,7 @@ export default function HeadlineResponse() {
   return <div className="min-h-screen bg-background">
       <Header />
       
-      <main className="container mx-auto px-4 pt-24 pb-12">
+      <main className="container mx-auto px-6 py-6">
         <Breadcrumb />
         
         {/* Interaction Mode Toggle */}
